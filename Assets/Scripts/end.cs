9d1385cd34fb6c274f94c20f538c7bcb828eb899
RobocodeObolon <>
2025-06-29 17:54:54 +0300

    l

---
+++ Assets/Scripts/end.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class end : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
